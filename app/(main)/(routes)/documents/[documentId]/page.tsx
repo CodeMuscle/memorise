042706105232eda54bda83684d592015e825018e
CodeMuscle <investportal99@gmail.com>
2024-01-11 02:45:27 +0530

    Add Banner Component, Toolbar Component

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -4,6 +4,7 @@ import { useQuery } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
+import Toolbar from "@/components/toolbar";
 
 interface DocumentPageProps {
   params: {
@@ -13,11 +14,32 @@ interface DocumentPageProps {
 
 const InnerDocumentPage = ({ params }: DocumentPageProps) => {
 
-  
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId
+  });  
+
+  if(document === undefined){
+    return(
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
+  if(document === null){
+    return(
+      <div className="">
+        Not found...
+      </div>
+    )
+  }
 
   return (
-    <div className="">
-      
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
     </div>
   )
 }
